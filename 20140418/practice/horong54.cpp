dec83a85435891eec0196aff83a833a3915a2ea6
Hoyoung Jung <k502000@naver.com>
2014-04-21 20:54:55 +0900

    Update horong54.cpp
    remove code redundancy.

--- 20140418/practice/horong54.cpp
+++ 20140418/practice/horong54.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int searchInsert(int A[], int n, int target) {
-        int left, right, mid, ret;
-        left = ret = 0;
+        int left, right, mid;
+        left = 0;
         right = n - 1;
         while (left <= right) {
             mid = (left + right) / 2;
             if (A[mid] == target)
                 return mid;
-            else if (target > A[mid] ) {
+            else if (target > A[mid] ) 
                 left = mid + 1;
-                ret = left;
-            }
-            else {
+            else 
                 right = mid - 1;
-                ret = mid;
-            }
         }
-        return ret;
+        if (target > A[mid])
+            return mid + 1;
+        else 
+            return mid;
     }
 };
